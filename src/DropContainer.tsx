c5a1c4e6a5440e25315c6117556209be470be230
Cyklan <37187056+Cyklan@users.noreply.github.com37187056+Cyklan@users.noreply.github.com>
2021-05-14 16:03:05 +0200

    More user feedback

--- src/DropContainer.tsx
+++ src/DropContainer.tsx
@@ -1,24 +1,36 @@
-import { DragEvent, useState } from "react";
+import { DragEvent, useEffect, useState } from "react";
 import "./DropContainer.css";
 import useConverter from "./useConverter";
 
 export function DropContainer() {
 
   const [file, setFile] = useState<File>();
-  useConverter(file);
+  const [message, setMessage] = useState("DROP FILE HERE");
+  const converting = useConverter(file);
 
   function preventDefault(event: DragEvent) {
     event.preventDefault();
   }
 
+  useEffect(() => {
+    setMessage(converting ? "CONVERTING..." : "DROP FILE HERE");
+  }, [converting]);
+
   return (
     <div className="dropcontainer" onDragOver={preventDefault} onDragEnter={preventDefault} onDrop={event => {
       event.preventDefault();
       const files = event.dataTransfer.files;
-      if (files.length > 1) return;
+      if (files.length !== 1) {
+        setMessage("ONLY CONVERT ONE FILE");
+        return;
+      }
+      if (files[0].size > 200 * 1024 * 1024) {
+        setMessage("MAX FILE SIZE: 200MB");
+        return;
+      }
       setFile(files[0]);
     }}>
-      DROP FILE HERE
+      {message}
     </div>
   );
 }
--- src/useConverter.ts
+++ src/useConverter.ts
@@ -1,22 +1,28 @@
 import FileSaver from "file-saver";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { decode, encode } from "./converter";
 
 export default function useConverter(file?: File) {
+
+  const [converting, setConverting] = useState(false);
+
   useEffect(() => {
     if (file == null) {
       return;
     }
-    console.log(file.name);
+    setConverting(true);
     if (file.name.endsWith(".png")) {
       decode(file)
-        .then(res => FileSaver.saveAs(new Blob([res.bytes]), res.fileName));
+        .then(res => FileSaver.saveAs(new Blob([res.bytes]), res.fileName))
+        .then(() => setConverting(false));
       return;
     }
 
     encode(file)
       .then(png => {
         FileSaver.saveAs(new Blob([png]), file.name + ".png");
-      });
+      }).then(() => setConverting(false));
   }, [file]);
+
+  return converting;
 }
